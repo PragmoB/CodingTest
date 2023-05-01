328cb763604db54cb3f0674f50831cf4e596b61d
PragmoB <peullaegeumo@gmail.com>
2023-05-02 01:09:43 +0900

    solve 2098

---
+++ 백준/역대 DP 연습셋 기출문제/줄세우기/줄세우기.cpp
@@ -0,0 +1,29 @@
+﻿// 줄세우기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	uint32_t N;
+	uint32_t input[200];
+	
+	cin >> N;
+	
+	for (uint32_t i = 0; i < N; i++)
+		cin >> input[i];
+
+	uint32_t current_max = 0, result = 0;
+
+	for (uint32_t i = 0; i < N; i++)
+	{
+		if (current_max < input[i])
+			current_max = input[i];
+		else
+			result++;
+	}
+
+	cout << result;
+}
